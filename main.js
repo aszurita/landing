bab19963abf1242c51d771297b435aa7c6502c33
aszurita <nzuritag@fiec.espol.edu.ec>
2024-12-01 13:56:15 -0500

    Sección Innovaciones terminada

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+    once: false,
+    duration: 1000,
+    offset: 100,
+    delay: 0,
+    easing: 'ease-in-out-back'
+  });
+});
